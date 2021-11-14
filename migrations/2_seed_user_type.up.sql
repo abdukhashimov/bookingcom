917541dc74f838a177b0cc872d90fc4eb79ffffa
Madiyorr Abdukhashimov <abdukhashimov@yandex.ru>
2021-11-14 13:43:23 +0500

    fix: add missing table struct - syntax error

--- migrations/2_seed_user_type.up.sql
+++ migrations/2_seed_user_type.up.sql
@@ -1,4 +1,4 @@
 -- inserting into user_type - basic user types
-INSERT INTO user_type (1, 'customer');
-INSERT INTO user_type (2, 'owner');
-INSERT INTO user_type (777, 'admin');
+INSERT INTO user_type (id, name) VALUES (1, 'customer');
+INSERT INTO user_type (id, name) VALUES (2, 'owner');
+INSERT INTO user_type (id, name) VALUES (777, 'admin');
