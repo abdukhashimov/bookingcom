9374d9b861830ff4838ee3c013227675e9f3ed31
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-11-14 21:10:44 +0500

    fix: add queries

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "abdukhashimov/mybron.uz/storage/sqlc"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Queries *sqlc.Queries
+}
