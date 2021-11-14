6fdbcb04a885b01fd2072f9171e69c99ea15f6a1
Madiyorr Abdukhashimov <abdukhashimov@yandex.ru>
2021-11-14 13:41:39 +0500

    fix: add booking test database connectyion

---
+++ handler/main_test.go
@@ -0,0 +1,36 @@
+package handler_test
+
+import (
+	"abdukhashimov/mybron.uz/storage/sqlc"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	postgresConn *sql.DB
+	err          error
+	queries      *sqlc.Queries
+)
+
+func TestMain(m *testing.M) {
+	conStr := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s",
+		"localhost",
+		5432,
+		"postgres",
+		"postgres",
+		"booking",
+		"disable",
+	)
+
+	postgresConn, err = sql.Open("postgres", conStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queries = sqlc.New(postgresConn)
+	os.Exit(m.Run())
+}
