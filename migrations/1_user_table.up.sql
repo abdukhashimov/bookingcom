cd4bde9b37afa9370431be3239e4dc669b9782cf
Madiyorr Abdukhashimov <abdukhashimov@yandex.ru>
2021-11-14 09:52:48 +0500

    fix: add basic admin types

--- migrations/1_user_table.up.sql
+++ migrations/1_user_table.up.sql
@@ -14,3 +14,8 @@ CREATE TABLE "users" (
   "created_at" timestamp,
   "updated_at" timestamp
 );
+
+-- inserting into user_type - basic user types
+INSERT INTO user_type (1, 'customer');
+INSERT INTO user_type (2, 'owner');
+INSERT INTO user_type (777, 'admin');
