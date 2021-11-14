058bebec67d7cb4be970bcead22af63c673fc3b0
Madiyorr Abdukhashimov <abdukhashimov@yandex.ru>
2021-11-14 13:46:08 +0500

    feat: add test to retreive all users

--- handler/user_test.go
+++ handler/user_test.go
@@ -1,7 +1,21 @@
 package handler_test
 
-import "testing"
+import (
+	"abdukhashimov/mybron.uz/handler"
+	"abdukhashimov/mybron.uz/storage/sqlc"
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGetAllUsers(t *testing.T) {
-
+	userHandler := handler.NewUserHandler(queries)
+	users, err := userHandler.GetAll(context.Background(), sqlc.GetUsersParams{
+		Offset: 0,
+		Limit:  10,
+	})
+	assert.NoError(t, err, "Failed to retreive all users")
+	fmt.Println(users)
 }
