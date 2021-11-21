1aecb1dfd9f1626b12438e5768399eae15ea6540
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-11-21 10:44:40 +0500

    feat: add function footprints for authorization parsing

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -5,3 +5,17 @@ import "abdukhashimov/mybron.uz/config"
 type Jwt struct {
 	cfg *config.Config
 }
+
+type TokenPayload struct {
+	UserID string `json:"user_id"`
+}
+
+func (j *Jwt) GenerateToken(payload TokenPayload) (string, error) {
+
+	return "", nil
+}
+
+func (j *Jwt) ParseToken(tokenStr string) (TokenPayload, error) {
+
+	return TokenPayload{}, nil
+}
