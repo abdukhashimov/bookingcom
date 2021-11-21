1e10fa858e9743075b684cbee17b8f930b67f64e
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-11-21 10:52:23 +0500

    feat: implement generate token method

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,6 +1,11 @@
 package jwt
 
-import "abdukhashimov/mybron.uz/config"
+import (
+	"abdukhashimov/mybron.uz/config"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Jwt struct {
 	cfg *config.Config
@@ -11,8 +16,23 @@ type TokenPayload struct {
 }
 
 func (j *Jwt) GenerateToken(payload TokenPayload) (string, error) {
+	var (
+		tokenString string
+		err         error
+	)
 
-	return "", nil
+	token := jwt.New(jwt.SigningMethodES256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	// set claims
+	claims["payload"] = payload
+	claims["expires_at"] = time.Now().Add(
+		time.Hour * time.Duration(j.cfg.TokenExpireHour),
+	).Unix()
+
+	tokenString, err = token.SignedString(j.cfg.JWTSecretKey)
+
+	return tokenString, err
 }
 
 func (j *Jwt) ParseToken(tokenStr string) (TokenPayload, error) {
