8826b9ba19524dc55ee314b35d02b803cf7cc2d6
Madiyor <madiyar.abdukhashimov@udevs.io>
2021-11-21 13:01:19 +0500

    fix: add debug and warn logs

--- pkg/auth/main.go
+++ pkg/auth/main.go
@@ -16,14 +16,17 @@ func (a *Auth) MiddleWare() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 			authorizationHeader := r.Header.Get("Authorization")
+			a.log.Debug("auth middleware is running", logger.String("Authorization", authorizationHeader))
 
 			if authorizationHeader == "" {
+				a.log.Warn("no authorization header is provided")
 				next.ServeHTTP(rw, r)
 				return
 			}
 
 			tokenPayload, err := a.jwt.ParseToken(authorizationHeader)
 			if err != nil {
+				a.log.Warn("authorization token is invalid")
 				http.Error(rw, "Authorization token is invalid", http.StatusForbidden)
 				return
 			}
