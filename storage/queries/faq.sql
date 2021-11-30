6e3b66364b59e0b50589a36e1683a8a48550bf84
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 12:04:41 +0500

    fix: change fields

--- storage/queries/faq.sql
+++ storage/queries/faq.sql
@@ -16,28 +16,24 @@ LIMIT $2;
 -- name: CreateFaq :one
 INSERT INTO faq (
         id,
-        first_name,
-        last_name,
-        phone_number,
-        is_verified,
-        long,
-        lat,
-        user_type,
+        question,
+        answer,
+        slug,
+        lang,
+        active,
         created_at,
         updated_at
     )
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING *;
 
 -- name: UpdateFaq :exec
 UPDATE faq
-SET first_name = COALESCE(@first_name, first_name),
-    last_name = COALESCE(@second_name, last_name),
-    phone_number = COALESCE(NULLIF(@phone_number, ''), phone_number),
-    is_verified = COALESCE(@is_verified, is_verified),
-    long = COALESCE(@long, long),
-    lat = COALESCE(@lat, lat),
-    user_type = COALESCE(@user_type, user_type),
+SET question = COALESCE(@question, question),
+    answer = COALESCE(@answer, answer),
+    slug = COALESCE(@slug, '', slug),
+    lang = COALESCE(@lang, lang),
+    active = COALESCE(@active, active),
     updated_at = COALESCE(@updated_at, updated_at)
 WHERE id = @id;
 
