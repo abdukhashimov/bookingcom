5dd0895c131d028c5bf125dc7fe353495a035a25
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 12:50:53 +0500

    feat: start with create faq

--- services/faq.go
+++ services/faq.go
@@ -1,11 +1,17 @@
 package services
 
 import (
+	"abdukhashimov/mybron.uz/graph/model"
 	"abdukhashimov/mybron.uz/pkg/jwt"
 	"abdukhashimov/mybron.uz/storage/sqlc"
+	"context"
 )
 
 type faqService struct {
 	db  *sqlc.Queries
 	jwt jwt.Jwt
+}
+
+func (f *faqService) CreateFaq(ctx context.Context, req model.CreateFaq) (*model.Faq, error) {
+	return nil, nil
 }
