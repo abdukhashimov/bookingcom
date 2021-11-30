9b2ec0320bde30bac3fa6a7b698ca7f21f663645
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 17:43:53 +0500

    fix: add category create

--- services/category.go
+++ services/category.go
@@ -4,6 +4,7 @@ import (
 	"abdukhashimov/mybron.uz/graph/model"
 	"abdukhashimov/mybron.uz/pkg/jwt"
 	"abdukhashimov/mybron.uz/storage/sqlc"
+	"context"
 )
 
 type categoryService struct {
@@ -18,22 +19,22 @@ func NewCategoryService(db *sqlc.Queries, jwt jwt.Jwt) *categoryService {
 	}
 }
 
-func (c *categoryService) CreateCategory() (*model.Category, error) {
+func (c *categoryService) CreateCategory(ctx context.Context, req model.CreateCategory) (*model.Category, error) {
 	return nil, nil
 }
 
-func (c *categoryService) UpdateCategory() (*model.Category, error) {
+func (c *categoryService) UpdateCategory(ctx context.Context, req model.UpdateCategory) (*model.Category, error) {
 	return nil, nil
 }
 
-func (c *categoryService) DeleteCategory() (string, error) {
+func (c *categoryService) DeleteCategory(ctx context.Context, slug string) (string, error) {
 	return "", nil
 }
 
-func (c *categoryService) GetCategory() (*model.Category, error) {
+func (c *categoryService) GetCategory(ctx context.Context, slug, lang string) (*model.Category, error) {
 	return nil, nil
 }
 
-func (c *categoryService) GetAllCategory() (*model.GetAllCategory, error) {
+func (c *categoryService) GetAllCategory(ctx context.Context, lang string, limit, offset *int) (*model.GetAllCategory, error) {
 	return nil, nil
 }
