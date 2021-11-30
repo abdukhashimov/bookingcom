4c0f542840c235c60a4cb11048270e6bac4dd9ac
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 13:21:35 +0500

    fix: add create faq

--- services/faq.go
+++ services/faq.go
@@ -1,12 +1,18 @@
 package services
 
 import (
+	"abdukhashimov/mybron.uz/config"
 	"abdukhashimov/mybron.uz/graph/model"
 	"abdukhashimov/mybron.uz/pkg/jwt"
+	"abdukhashimov/mybron.uz/pkg/utils"
 	"abdukhashimov/mybron.uz/storage/sqlc"
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/gosimple/slug"
+	"github.com/teris-io/shortid"
 )
 
 type faqService struct {
@@ -25,20 +31,31 @@ func (f *faqService) CreateFaq(ctx context.Context, req model.CreateFaq) (*model
 	var (
 		payload  sqlc.CreateFaqParams
 		response model.Faq
+		res      sqlc.Faq
+		err      error
 	)
 
+	shortId, err := shortid.Generate()
+	if err != nil {
+		return &response, errors.New("internal server error")
+	}
+
 	payload.ID = uuid.NewString()
-	err := modelToStruct(req, payload)
+	payload.Slug = slug.Make(fmt.Sprintf("%s-%s", utils.FirstN(*payload.Question), shortId))
+
+	err = modelToStruct(req, payload)
 	if err != nil {
 		return &response, err
 	}
 
-	res, err := f.db.CreateFaq(ctx, payload)
-	if err != nil {
-		return &response, err
+	for _, lang := range config.Langs {
+		payload.Lang = lang
+		res, err = f.db.CreateFaq(ctx, payload)
+		if err != nil {
+			return &response, err
+		}
 	}
 
 	err = modelToStruct(res, &response)
-
 	return &response, err
 }
