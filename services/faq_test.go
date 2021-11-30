695722efcd0bb1a64af9ced7f53934ae79431829
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 15:15:19 +0500

    fix: add delete method

--- services/faq_test.go
+++ services/faq_test.go
@@ -4,10 +4,10 @@ import (
 	"abdukhashimov/mybron.uz/graph/model"
 	"abdukhashimov/mybron.uz/pkg/logger"
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/bxcodec/faker/v3"
+	"github.com/stretchr/testify/assert"
 )
 
 func createFAQ(t *testing.T) *model.Faq {
@@ -27,5 +27,14 @@ func createFAQ(t *testing.T) *model.Faq {
 
 func TestCreateFAQ(t *testing.T) {
 	faq := createFAQ(t)
-	fmt.Println(faq)
+	deleteFaq(t, faq.Slug)
+}
+
+func deleteFaq(t *testing.T, slug string) {
+	_, err := servcesObj.FaqService().DeleteFaq(context.Background(), slug)
+	assert.NoError(t, err, "failed to delete faq")
+	if err != nil {
+		log.Fatal("error while deleting faq", logger.Error(err))
+		return
+	}
 }
