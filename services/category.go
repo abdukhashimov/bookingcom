d27aac7b00260e3972c3c568febf496a7e2a7868
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 17:49:31 +0500

    feat: add create category

--- services/category.go
+++ services/category.go
@@ -1,10 +1,18 @@
 package services
 
 import (
+	"abdukhashimov/mybron.uz/config"
 	"abdukhashimov/mybron.uz/graph/model"
 	"abdukhashimov/mybron.uz/pkg/jwt"
+	"abdukhashimov/mybron.uz/pkg/utils"
 	"abdukhashimov/mybron.uz/storage/sqlc"
 	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/gosimple/slug"
+	"github.com/teris-io/shortid"
 )
 
 type categoryService struct {
@@ -20,7 +28,36 @@ func NewCategoryService(db *sqlc.Queries, jwt jwt.Jwt) *categoryService {
 }
 
 func (c *categoryService) CreateCategory(ctx context.Context, req model.CreateCategory) (*model.Category, error) {
-	return nil, nil
+	var (
+		payload  sqlc.CreateCategoryParams
+		response model.Category
+		res      sqlc.Category
+		err      error
+	)
+
+	shortId, err := shortid.Generate()
+	if err != nil {
+		return &response, errors.New("internal server error")
+	}
+
+	payload.Slug = slug.Make(fmt.Sprintf("%s-%s", utils.FirstN(req.Name), shortId))
+
+	err = modelToStruct(req, &payload)
+	if err != nil {
+		return &response, err
+	}
+
+	for _, lang := range config.Langs {
+		payload.ID = uuid.NewString()
+		payload.Lang = lang
+		res, err = c.db.CreateCategory(ctx, payload)
+		if err != nil {
+			return &response, err
+		}
+	}
+
+	err = modelToStruct(res, &response)
+	return &response, err
 }
 
 func (c *categoryService) UpdateCategory(ctx context.Context, req model.UpdateCategory) (*model.Category, error) {
