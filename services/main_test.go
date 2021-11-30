5f3ce4630fc2130dce5e9bd7de417509a846ff7e
Madiyor <abdukhashimov@yandex.ru>
2021-11-30 12:29:44 +0500

    fix: add logger log

--- services/main_test.go
+++ services/main_test.go
@@ -1,10 +1,12 @@
 package services_test
 
 import (
+	"abdukhashimov/mybron.uz/config"
+	"abdukhashimov/mybron.uz/pkg/jwt"
+	"abdukhashimov/mybron.uz/pkg/logger"
 	"abdukhashimov/mybron.uz/storage/sqlc"
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 	"testing"
 
@@ -15,9 +17,14 @@ var (
 	postgresConn *sql.DB
 	err          error
 	queries      *sqlc.Queries
+	cfg          *config.Config
+	services     services.Services
+	log          logger.Logger
 )
 
 func TestMain(m *testing.M) {
+	cfg = config.NewConfig()
+	log = logger.New("debug", "test-my-bron.uz")
 	conStr := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s",
 		"localhost",
 		5432,
@@ -30,9 +37,10 @@ func TestMain(m *testing.M) {
 	postgresConn, err = sql.Open("postgres", conStr)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("error while connecting to postgres", logger.Error(err))
 	}
 
 	queries = sqlc.New(postgresConn)
+	services = services.NewServices(queries, jwt.NewJwt(cfg, log))
 	os.Exit(m.Run())
 }
